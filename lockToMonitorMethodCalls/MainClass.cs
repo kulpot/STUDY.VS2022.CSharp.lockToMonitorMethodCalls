dfee98331342ff4e6e904126f855cfb45cdcbae0
Kulpot <kulpotwahu@gmail.com>
2023-07-17 06:47:32 +0800

    Add project files.

---
+++ lockToMonitorMethodCalls/MainClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+//ref link:https://www.youtube.com/watch?v=cQadZLH8NkI&list=PLRwVmtr-pp06KcX24ycbC-KkmAISAFKV5&index=10
+//
+
+class MainClass
+{
+    static object baton = new object();
+    static void Main()
+    {
+
+        /*bool lockTaken = false;
+        Monitor.Enter(baton, ref lockTaken);
+        try
+        {
+            Console.WriteLine("Got the baton");
+        }
+        finally 
+        { 
+            if (lockTaken)
+                Monitor.Exit(baton);
+        }*/
+        lock (baton)
+        {
+            Console.WriteLine("Got the baton");
+        }
+    }
+}
